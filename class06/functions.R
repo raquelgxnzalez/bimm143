455221f20f0d6489c2a4b3c75d5c6be8401dd49c
Raquel Gonzalez <raqueliegonzalez02@gmail.com>
2024-03-14 10:36:34 -0700

    add class 6

---
+++ class06/functions.R
@@ -0,0 +1,31 @@
+add <- function(x, y=1) {
+  x + y
+}
+
+add(x=4)
+
+add(x=4, y=10)
+
+add(4, 20)
+
+add(c(1, 2, 5, 10), y=10)
+
+# Our 2nd function rescale()
+rescale <- function(x) {
+  rng <- range(x)
+  (x-rng[1])/(rng[2]-rng[1])
+}
+
+# Test on a simple example
+rescale(1:10)
+
+rescale( c(1, NA, 4, 10, 20) )
+
+# Fix the NA issue
+rescale01 <- function(x) {
+  rng <- range(x, na.rm=TRUE)
+  (x-rng[1])/(rng[2]-rng[1])
+}
+
+rescale01(c(1, NA, 4, 10, 20))
+
